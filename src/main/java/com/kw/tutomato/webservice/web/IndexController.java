8c443f5b37cbe790289d4aaa404dc0f5acba2dd1
jongwanKim <jongbell4@gmail.com>
2021-11-02 16:12:32 +0900

    1. .gitignore 수정  - 이그노어 파일 수정 및 업로드 파일 변경을 위해 repository 재생성  -> 기존 작업 history가 유지 되지 않는 문제 ( 기존 commit history 보존하며 repository 변경하는 방법 찾아 볼 필요 )
    2. Google OAuth2 로그인 구현
     - google cloud platform oauth2 로그인 구현

--- src/main/java/com/kw/tutomato/webservice/web/IndexController.java
+++ src/main/java/com/kw/tutomato/webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kw.tutomato.webservice.web;
 
+import com.kw.tutomato.webservice.config.oauth.dto.SessionUser;
 import com.kw.tutomato.webservice.service.PostsService;
 import com.kw.tutomato.webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -10,17 +11,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         log.debug("findAllDesc :: {}", postsService.findAllDesc());
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
