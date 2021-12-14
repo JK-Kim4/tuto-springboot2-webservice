d782258a9240f9f88e343bece80c8cb798a498a5
jongwan <jongbell4@gmail.com>
2021-12-15 02:55:59 +0900

    무중단 배포 적용

--- src/main/java/com/kw/tutomato/webservice/config/oauth/SecurityConfig.java
+++ src/main/java/com/kw/tutomato/webservice/config/oauth/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/user/update/state").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/user/update/state", "/profile").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
