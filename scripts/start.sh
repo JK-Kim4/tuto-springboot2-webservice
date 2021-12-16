1afe555336a456e470b7e824ad7013ed1db3d0fd
jongwan <jongbell4@gmail.com>
2021-12-17 01:13:52 +0900

    스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -27,6 +27,6 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME를 profile=$IDLE_PROFILE로 실행합니다."
 nohup java -jar \
-  -DSpring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -DSpring.profile.active=$IDLE_PROFILE \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
